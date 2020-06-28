d30ba5b5520ed241c9164bd2c64de4f6d4b72095
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-06-27 18:39:36 -0600

    added logic for building deck, created html boilerplate

--- src/components/deck.js
+++ src/components/deck.js
@@ -1,3 +1,27 @@
 class Deck {
-    
+
+
+
+    static createDeck() {
+
+        let suits = ["Clubs", "Hearts", "Spades", "Diamonds"];
+        let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+        var deck = []
+
+        for (var i = 0 ; i < values.length; i++) {
+
+            for(var x = 0; x < suits.length; x++) {
+
+                var weight = parseInt(values[i]);
+                if (values[i] == "J" || values[i] == "Q" || values[i] == "K")
+                    weight = 10;
+                if (values[i] == "A")
+                    weight = 11;
+                var card = { Value: values[i], Suit: suits[x], Weight: weight };
+                deck.push(card);
+            }
+        }
+        return deck
+    }
+
 }
