c2f344460f9d9b7b6c6f5e18636c6d87e5c61851
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-06-28 19:18:26 -0600

    built shuffle deck function

--- src/components/deck.js
+++ src/components/deck.js
@@ -1,7 +1,6 @@
 class Deck {
 
 
-
     static buildDeck() {
 
         const suits = ["Clubs", "Hearts", "Spades", "Diamonds"];
@@ -22,6 +21,17 @@ class Deck {
             }
         }
         return deck
+    }
+
+    static shuffleDeck(array) {
+        const i = 0, j = 0, temp = null
+      
+        for (i = array.length - 1; i > 0; i -= 1) {
+          j = Math.floor(Math.random() * (i + 1))
+          temp = array[i]
+          array[i] = array[j]
+          array[j] = temp
+        }
     }
 
 }
