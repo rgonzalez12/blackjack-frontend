dc219ccae0c88a61b42208c4bccb931ced9ffe25
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-07-11 21:44:07 -0600

    added more dom building functions/wiring

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     let newGameButton = document.getElementById('new-game-button');
     newGameButton.addEventListener('click', function() {
         const name_input = document.getElementById('name-input')
-        const player = new Player(name_input.value);
         const dealer = new Player('Dealer')
+        const player = new Player(name_input.value);
         
         const game = new Game(1, 0, 0, 0, player, dealer, { debugOptions: ['HIT', 'HIT', 'HIT'] });
         name_input.value = ""
@@ -11,28 +11,51 @@ document.addEventListener("DOMContentLoaded", () => {
     
         game.startGame();
         const playerHand = game.user.currentHand
+        const playerHit = game.hitCard
+        const playerStay = game.stay
+        const dealerHand = game.dealer.currentHand
 
         renderPlayerHand(playerHand)
+        renderDealerHand(dealerHand)
         renderPlayerScore(game.user.currentScore())
-        renderHitButton(game.user.hitCard(playerThatHit))
-        renderStayButton(game.user.stay(user))
-    
+        renderHitButton(playerHit)
+        renderStayButton(playerStay)
       })
 })
 
 const renderPlayerHand = (playerHand) => {
     let gameSection = document.getElementById('game-section')
-    let node = document.createElement("LI")
-    let node2 = document.createElement("LI")
+    let node = document.createElement("H5")
+    let node2 = document.createElement("H5")
+    let playerCards = document.createTextNode(`Your Hand:`)
     let playerCardOne = document.createTextNode(`${playerHand[0].rank} of ${playerHand[0].suit}`)
     node.appendChild(playerCardOne)
 
     let playerCardTwo = document.createTextNode(`${playerHand[1].rank} of ${playerHand[1].suit}`)
     node2.appendChild(playerCardTwo)
 
+    gameSection.appendChild(playerCards)
+    gameSection.appendChild(node)
+    gameSection.appendChild(node2)
+    
+}
+
+const renderDealerHand = (dealerHand) => {
+    let gameSection = document.getElementById('game-section')
+    let node = document.createElement("h5")
+    let node2 = document.createElement("h5")
+    let dealerCards = document.createTextNode(`Dealer Hand:`)
+    let dealerCardOne = document.createTextNode(`${dealerHand[0].rank} of ${dealerHand[0].suit}`)
+    node.appendChild(dealerCardOne)
+
+    let dealerCardTwo = document.createTextNode(`${dealerHand[1].rank} of ${dealerHand[1].suit}`)
+    node2.appendChild(dealerCardTwo)
+
+    gameSection.appendChild(dealerCards)
     gameSection.appendChild(node)
     gameSection.appendChild(node2)
 }
+
 
 const renderPlayerScore = (currentScore) => {
     let gameSection = document.getElementById('game-section')
@@ -55,7 +78,7 @@ const renderHitButton = (hitCard) => {
 const renderStayButton = (stay) => {
     let gameSection = document.getElementById('game-section')
     let node = document.createElement("BUTTON")
-    let playerStay = document.createElement(`Stay`)
+    let playerStay = document.createTextNode(`Stay`)
 
     node.appendChild(playerStay)
     gameSection.appendChild(node)
